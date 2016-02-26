b1537af57f14b99068f0feb8b8ccd14398b3b6af
Joaquim Rocha <jrocha@gmailbox.org>
2016-02-26 00:24:31 +0000

    Change generator

--- src/narciso.erl
+++ src/narciso.erl
@@ -16,21 +16,27 @@
 
 -module(narciso).
 
+-define(UUID_VERSION, 2#0100).
+-define(UUID_VARIANT, 2#10).
+
 %% ====================================================================
-%% API functions
+%% API
 %% ====================================================================
 -export([unique/0, uuid/0, token/0, id/0]).
 -export([uuid/1, token/1, id/1]).
 -export([uuid_to_unique/1, token_to_unique/1, id_to_unique/1]).
 -export([is_unique/1, is_uuid/1, is_token/1, is_id/1]).
 
+%% ====================================================================
+%% Generation functions
+%% ====================================================================
 -spec unique() -> binary().
 unique() ->
-	[A, B, C, D, E] = v4(),
-	<<A:32, B:16, C:16, D:16, E:48>>.
+	<<AB:48, _:4, C:12, _:2, DE:62>> = crypto:rand_bytes(16),
+	<<AB:48, ?UUID_VERSION:4, C:12, ?UUID_VARIANT:2, DE:62>>.
 
 -spec uuid() -> binary().
-uuid() -> uuid(v4()).
+uuid() -> uuid(unique()).
 
 -spec token() -> binary().
 token() -> token(unique()).
@@ -38,9 +44,11 @@ token() -> token(unique()).
 -spec id() -> integer().
 id() -> id(unique()).
 
+%% ====================================================================
+%% Generation of variants using unique
+%% ====================================================================
 -spec uuid(Unique :: binary()) -> binary().	
-uuid(<<A:32, B:16, C:16, D:16, E:48>>) -> uuid([A, B, C, D, E]);
-uuid([A, B, C, D, E]) ->
+uuid(<<A:32, B:16, C:16, D:16, E:48>>) ->
 	UUID = io_lib:format("~8.16.0b-~4.16.0b-~4.16.0b-~4.16.0b-~12.16.0b", [A, B, C, D, E]),
 	list_to_binary(UUID).
 
@@ -52,6 +60,9 @@ token(<<Id:128>>) ->
 -spec id(Unique :: binary()) -> integer().	
 id(<<Id:128>>) -> Id.
 
+%% ====================================================================
+%% Parsing from variants to unique
+%% ====================================================================
 -spec uuid_to_unique(Uuid :: binary()) -> binary().	
 uuid_to_unique(<<AA:64, _:8, BB:32, _:8, CC:32, _:8, DD:32, _:8, EE:96>>) ->
 	A = binary_to_integer(<<AA:64>>, 16),
@@ -69,15 +80,19 @@ token_to_unique(Token) ->
 -spec id_to_unique(Id :: integer()) -> binary().	
 id_to_unique(Id) -> <<Id:128>>.	
 
+%% ====================================================================
+%% Validation functions
+%% ====================================================================
 -spec is_unique(Unique :: binary()) -> boolean().	
-is_unique(<<_:48, C:16, D:16, _:48>>) ->
-	between(C, 16#4000, 16#4fff) andalso between(D, 16#8000, 16#8fff);
+is_unique(<<_:48, Version:4, _:12, Variant:2, _:62>>) when Version =:= ?UUID_VERSION andalso Variant =:= ?UUID_VARIANT -> true;
 is_unique(_) -> false.
 
 -spec is_uuid(UUID :: binary()) -> boolean().
 is_uuid(UUID = <<_:64, $-, _:32, $-, _:32, $-, _:32, $-, _:96>>) ->
-	case re:run(UUID, <<"[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-8[0-9a-f]{3}-[0-9a-f]{12}">>) of
-		{match, _} -> true;
+	case re:run(UUID, <<"[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[0-9a-f]{4}-[0-9a-f]{12}">>) of
+		{match, _} -> 
+			Unique = uuid_to_unique(UUID),
+			is_unique(Unique);
 		_ -> false
 	end;
 is_uuid(_) -> false.
@@ -86,34 +101,14 @@ is_uuid(_) -> false.
 is_token(Token) when is_binary(Token) ->
 	case re:run(Token, <<"^([0-9a-z]+)$">>) of
 		{match, _} -> 
-			Id = binary_to_integer(Token, 36),
-			is_id(Id);
+			Unique = token_to_unique(Token),
+			is_unique(Unique);
 		_ -> false
 	end;
 is_token(_) -> false.
 
 -spec is_id(Id :: integer()) -> boolean().
-is_id(Id) when is_integer(Id) andalso Id =< 16#ffffffffffffffffffffffffffffffff -> is_unique(<<Id:128>>);
+is_id(Id) when is_integer(Id) andalso Id =< 16#ffffffffffffffffffffffffffffffff -> 
+	Unique = id_to_unique(Id),
+	is_unique(Unique);
 is_id(_) -> false.
-
-%% ====================================================================
-%% Internal functions
-%% ====================================================================
-v4() ->
-	Time = erlang:phash2(utc_date(), 16#100000000),
-	Host = erlang:phash2(host_process(), 16#10000),
-	Rand1 = crypto:rand_uniform(16#4000, 16#5000),
-	Rand2 = crypto:rand_uniform(16#8000, 16#9000),
-	Rand3 = crypto:rand_uniform(0, 16#1000000000000),
-	[Time, Host, Rand1, Rand2, Rand3].
-
-utc_date() ->
-	TS = {_,_, Micro} = os:timestamp(),
-	{Date, Time} = calendar:now_to_universal_time(TS),
-	{Date, Time, Micro}.
-
-host_process() ->
-	{node(), self()}.
-
-between(Value, Lo, Hi) when Value >= Lo andalso Value =< Hi -> true;
-between(_, _, _) -> false.
